762e9d67afea68dcd40fc416e88d83ec4ca54910
Brian <bloughran618@gmail.com>
2019-05-07 23:04:23 -0400

    remove unneccessary debug

--- StripeBackend.py
+++ StripeBackend.py
@@ -3,6 +3,7 @@ import time
 from flask import Flask, session, jsonify, request, Response
 import sys
 import json
+import firebase
 
 # This NEEDS to be set to os.environ.get when we go to prod
 stripe.api_key = "sk_test_BPL2Sy81u9355r3GlN4XKG2t"
@@ -469,7 +470,7 @@ def fetch_from_firebase():
 # print("-------------------------------------------------------------------------\n"
 #       + str(account))
 
-try:
-    import firebase
-except Exception as e:
-    log_info(" This is the exception: \n" + str(e))
+# try:
+#     import firebase
+# except Exception as e:
+#     log_info(" This is the exception: \n" + str(e))
