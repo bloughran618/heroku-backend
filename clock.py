c4a3fac4837ced8000b62a724b031675995d14ce
Drew Loughran <dloughran295@gmail.com>
2019-07-17 11:10:17 -0400

    adding clock python file

--- clock.py
+++ clock.py
@@ -23,7 +23,9 @@ def timed_job():
 def my_job(text):
     print(text)
 
-sched.add_job(my_job, 'date', run_date='2019-7-17 11:08:00', args=['date job firing'], misfire_grace_time = 18000)
+sched.add_job(my_job, 'date', run_date='2019-7-17 11:15:00', args=['date job firing'], misfire_grace_time = 18000)
+sched.add_job(my_job, 'date', run_date='2019-7-17 12:30:00', args=['date to run after sleeping'], misfire_grace_time=18000)
+
 '''
 @sched.scheduled_job('date', run_date='2019-7-17 10:40:00', misfire_grace_time=18000)
 def date_job():
